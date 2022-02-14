d04c0694dbc9f431b61cb262eba9c87741f19fbb
Aracc <marko.peurala@tuni.fi>
2022-02-14 11:33:27 +0200

    fixed user inputs & physics

---
+++ Assets/code/BirdAi.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace kaalimaanvartija
+{
+    public class BirdAi : MonoBehaviour
+    {
+        private Vector2 startingPosition;
+
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+            startingPosition = transform.position;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
