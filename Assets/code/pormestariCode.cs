ee2cfb742108575b3a89e577f3874003f80c3549
joonaskiuru <joonas.kiuru@gmail.com>
2022-03-29 10:51:19 +0300

    scarecrow fix

--- Assets/code/pormestariCode.cs
+++ Assets/code/pormestariCode.cs
@@ -7,11 +7,13 @@ namespace kaalimaanvartija
     public class pormestariCode : MonoBehaviour
     {
         private GameObject canvas;
+        private GameObject HUD;
 
         void Awake()
         {
             Time.timeScale = 0.0f;
             canvas = GameObject.Find("PormestariCanvas");
+            HUD = GameObject.Find("HUD");
             canvas.SetActive(true);
         }
 
@@ -19,6 +21,7 @@ namespace kaalimaanvartija
         {
             Time.timeScale = 1.0f;
             canvas.SetActive(false);
+            HUD.SetActive(true);
             Destroy(canvas);
             Destroy(gameObject);
         }
