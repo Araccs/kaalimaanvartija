65a939c8283dc11e482b1eccaaf6ae4fe3c5569e
joonaskiuru <joonas.kiuru@gmail.com>
2022-03-30 12:00:54 +0300

    watering can

--- Assets/HeatwaveSpawn.cs
+++ Assets/HeatwaveSpawn.cs
@@ -12,6 +12,7 @@ namespace kaalimaanvartija
         [SerializeField] private float speed;
         private GameObject Tilemap;
         public Slider healthbar;
+        private bool destroyed;
 
         void Start()
         {
@@ -19,20 +20,33 @@ namespace kaalimaanvartija
             speed = speed / 100f;
             new_heatwave = Instantiate(heatwave, new Vector3(-25, 0, 0),Quaternion.identity);
             new_heatwave.transform.parent = Tilemap.transform;
+            destroyed = false;
         }
 
     // Update is called once per frame
         void FixedUpdate()
         {
-            new_heatwave.transform.position += new Vector3(speed, 0, 0);
-            if(new_heatwave.transform.position.x > 0 && new_heatwave.transform.position.x < 1)
+
+            if(destroyed == false)
             {
-                healthbar.value -= 1;
+
+                new_heatwave.transform.position += new Vector3(speed, 0, 0);
+                if (new_heatwave.transform.position.x > 0 && new_heatwave.transform.position.x < 1)
+                {
+                    healthbar.value -= 1;
+                }
+                else if (new_heatwave.transform.position.x > 20)
+                {
+                    Destroy(new_heatwave);
+                    destroyed = true;
+                    new_heatwave = Instantiate(heatwave, new Vector3(-25, 0, 0), Quaternion.identity);
+                    new_heatwave.transform.parent = Tilemap.transform;
+                    destroyed = false;
+                }
+
+
             }
-            else if (new_heatwave.transform.position.x > 20)
-            {
-                Destroy(new_heatwave);
-            }
+           
 
         }
     }
---
+++ Assets/Watering_can_script.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace kaalimaanvartija
+{
+    public class Watering_can_script : MonoBehaviour
+    {
+        private Button wateringCanButton;
+        public Slider healthslider;
+        private bool used;
+        private GameObject vegetable;
+        [SerializeField] private int time;
+        [SerializeField] private int healthBoost;
+        private float dist = 1f;
+        private Vector2 distance;
+        private GameObject player;
+        public Transform[] waterPoints = new Transform[4];
+        private Transform activeWaterPoint;
+        private bool isSpawned;
+        public GameObject waterObject;
+        private GameObject newWaterObject;
+
+
+        void Awake()
+        {
+            activeWaterPoint = waterPoints[Random.Range(0, 4)];
+            newWaterObject = Instantiate(waterObject, activeWaterPoint.position, Quaternion.identity);
+        }
+
+        public void OnClick()
+        {
+            wateringCanButton = GameObject.FindGameObjectWithTag("WateringCan").GetComponent<Button>();
+            vegetable = GameObject.FindGameObjectWithTag("Vegetable");
+            player = GameObject.FindGameObjectWithTag("Player");
+            Debug.Log(Vector2.Distance(player.transform.position, activeWaterPoint.transform.position));
+
+
+            if (used == false && Vector2.Distance(player.transform.position, activeWaterPoint.transform.position) <= dist)
+            {
+
+                healthslider.value += healthBoost;
+                used = true;
+                wateringCanButton.interactable = false;
+                Destroy(newWaterObject);
+                StartCoroutine(WaterCanTimer());
+            }
+
+
+        }
+
+        IEnumerator WaterCanTimer()
+        {
+
+            yield return new WaitForSeconds(time);
+            activeWaterPoint = waterPoints[Random.Range(0, waterPoints.Length-1)];
+            newWaterObject = Instantiate(waterObject, activeWaterPoint.position, Quaternion.identity);
+            used = false;
+            wateringCanButton.interactable = true;
+
+        }
+
+
+
+
+
+    }
+}
